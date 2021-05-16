24b4d5c02dddd28645afbc00476f00ec4515022a
Davide Salomoni <davide@infn.it>
2021-05-16 17:05:32 +0200

    updated files

---
+++ cloud_storage/get_pokemon.py
@@ -0,0 +1,10 @@
+import requests
+import json
+pokemon = requests.get('http://pokeapi.co/api/v2/pokemon/1/')
+ability = requests.get('http://pokeapi.co/api/v2/ability/1/')
+json_1 = json.loads(pokemon.content)
+json_2 = json.loads(ability.content)
+print("Pokemon Name: " + json_1['name'])
+print("Pokemon Ability: " + json_2['name'])
+print(json_1.keys())
+print(json_2.keys())
